1b46690f9d33e7d5ed6afd8ec5828952626c0656
Cecilia <barboza.ceciliam@gmail.com>
2024-09-13 18:38:40 -0400

    (Refactor): Install EsLint

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View,Text , SafeAreaView, TextInput, FlatList } from 'react-native';
+import { Text, SafeAreaView, TextInput, FlatList } from 'react-native';
 import { useState, useEffect } from 'react';
 import CardItem from '@/components/CardItem/CardItem'
 
@@ -6,71 +6,66 @@ import styles from '../styles/styles'
 
 
 type CatItem = {
+
   name: string;
   id: string;
-  
+
 };
 
 
-const CAT_API='https://api.thecatapi.com/v1/breeds';
+const CAT_API = 'https://api.thecatapi.com/v1/breeds';
 
 export default function HomeScreen() {
 
   const [inputText, setInputText] = useState('');
-  const [items, setItems]= useState<CatItem[]>([])
-  const [filteredItems, setFilteredItems] = useState<CatItem[]>([])
+  const [items, setItems] = useState<CatItem[]>([]);
+  const [filteredItems, setFilteredItems] = useState<CatItem[]>([]);
 
 
-  useEffect(()=>{
-    const fetchItems = async()=>{
-      try{
-      const response = await fetch(CAT_API)
-      const responseJson = await response.json()
-      
-      console.log(JSON.stringify(responseJson))
-      setItems(responseJson)
-      setFilteredItems(responseJson);
-      }catch(error){
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(CAT_API)
+        const responseJson = await response.json()
+
+        console.log(JSON.stringify(responseJson))
+        setItems(responseJson)
+        setFilteredItems(responseJson);
+      } catch (error) {
         console.error(error)
       }
     }
     fetchItems();
-  },[])
+  }, [])
 
   //filter funtionality
 
-  const filteredList = items.filter((items: CatItem)=>{
-          items.name.toLowerCase().includes(inputText.toLowerCase())
-        })
-
-  function handlerFilter(text: string){
-    if(text){
-      const filteredList = items.filter((item)=>
-      item.name.toUpperCase().includes(text.toUpperCase())
-    )
-    setFilteredItems(filteredList)
-
+  function handlerFilter(text: string) {
+    setInputText(text)
+    if (text) {
+      const filteredList = items.filter((item) =>
+        item.name.toUpperCase().includes(text.toUpperCase())
+      )
+      setFilteredItems(filteredList) // to show the filtered list
+    } else {
+      setFilteredItems(items) // to show the original list
     }
   }
-  
+
 
 
   return (
-      <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style={styles.titleContainer} >CatList</Text>
-      <TextInput style={styles.inputSearch} value={inputText} autoCapitalize='none' placeholder='search a cat' inlineImageLeft='search_icon' 
-        onChangeText={(text)=>{
-          setInputText(text) 
-          handlerFilter(text)
-          }
-        } 
+      <TextInput style={styles.inputSearch} value={inputText} autoCapitalize='none' placeholder='search a cat' inlineImageLeft='search_icon'
+        onChangeText={(text) => { handlerFilter(text) }}
         keyboardType='web-search' maxLength={20}/>
       <FlatList
-         keyExtractor={(item: CatItem)=>{return item.id}}
-         data={filteredItems} 
-         renderItem={({item}: {item: CatItem})=><CardItem name={item.name} />}
-         contentContainerStyle={{ gap: 15, width: 250, backgroundColor: 'yellow'}}
-        />
-      </SafeAreaView>
+        keyExtractor={(item: CatItem) => { return item.id }}
+        data={filteredItems}
+        renderItem={({ item }: { item: CatItem }) => <CardItem name={item.name} />}
+        contentContainerStyle={{ gap: 15, width: 250, backgroundColor: 'yellow' }}
+      />
+    </SafeAreaView>
   );
 }
